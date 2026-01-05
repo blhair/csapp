a776d76c0c2b382692c21c3250354e40d05aec16
hust-y <3266456420@qq.com>
2026-01-05 18:51:04 +0800

    增添了bomb实验文件

--- test.c
+++ test.c
@@ -1,19 +1,12 @@
-#include <stdio.h>
-
-int main() {
-    int uf = 0x800000;
-   int EMask = 0x7ffff;
-  int MMask = 0x7f80000;
-  if((uf&EMask) == EMask||uf==0 || uf == 0x80000000)
-  {
-    return uf;
-  }
-  else if ((uf&EMask) == 0)
-  {
-    int M = uf & MMask;
-    M = M << 1;
-    printf("%x %x", uf, M);
-    return (uf & ~MMask) + (M & MMask) + (M < 0);
-  }
-  return uf +1;
-}
+#include  <stdio.h> 
+void  multstore(long,  long,  long*); 
+int  main()  { 
+long  d; 
+multstore(2,  3,  &d); 
+printf("2  * 3 -->  %1d\n",  d); 
+return  0; 
+} 
+long  mult2(long  a,  long  b) { 
+long s= a* b; 
+return s; 
+} 
---
+++ testForExamples.c
@@ -0,0 +1,4 @@
+typedef int data_t;
+int  comp(data_t  a,  data_t  b ) { 
+return  a COMP b; 
+}
